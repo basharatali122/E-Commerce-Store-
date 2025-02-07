a980960069d70c70b5af97921f74e1c180b1fb38
basharatali112 <basharat62200150@gmail.com>
2025-02-07 23:12:36 +0500

    pushing code

---
+++ src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,81 @@
+import React, { useContext } from "react";
+import "./CartItems.css";
+import { ShopContext } from "../../Context/ShopContext";
+import remove_icon from "../../assets/cart_cross_icon.png";
+
+const CartItems = () => {
+  const { all_product, cartItems, removeCart, getTotalCartAmount } =
+    useContext(ShopContext);
+
+  return (
+    <div className="cartitems">
+      <div className="cartitems-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
+      {all_product.map((e) => {
+        if (cartItems[e.id] > 0) {
+          return (
+            <div key={e.id}>
+              <div className="cartitems-format cartitems-format-main">
+                <img src={e.image} className="carticon-product-icon" alt="" />
+                <p>{e.name}</p>
+                <p>${e.new_price}</p>
+                <button className="cartitems-quantity">
+                  {cartItems[e.id]}
+                </button>
+                <p>${e.new_price * cartItems[e.id]}</p>
+                <img
+                  className="cartitems-remove-icon"
+                  src={remove_icon}
+                  alt=""
+                  onClick={() => {
+                    removeCart(e.id);
+                  }}
+                />
+              </div>
+              <hr />
+            </div>
+          );
+        }
+        return null; // Added to handle the case when the if condition is not met
+      })}
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1>Cart Totals</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Shepping Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Total</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+          </div>
+          <button>PROCEED TO CHEKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+          <p>If you have a promo code, Enter it here</p>
+          <div className="cartitems-promobox">
+            <input type="text" placeholder="promo code" />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItems;
---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Descriptionbox.css";
+const DescriptionBox = () => {
+  return (
+    <div className="descriptioinbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade"> Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ducimus
+        excepturi, illo placeat veritatis magnam velit optio soluta facere
+        voluptatibus pariatur quos incidunt recusandae molestias ipsum explicabo
+        aut debitis maiores iste. Nobis necessitatibus excepturi amet possimus
+        veniam in beatae praesentium. Reiciendis vel quas suscipit? Ipsum beatae
+        voluptatibus fugiat? Expedita
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt illo
+          mollitia quidem facilis, minima hic accusantium repellendus voluptate
+          similique voluptatem omnis ducimus eveniet. Soluta, adipisci sed!
+          Dolorem veritatis ab magni.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
